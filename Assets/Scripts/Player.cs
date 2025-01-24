d30a70f338598bb39cbc1fc10d60756fa3bac927
Niko MBP <nikobranart@gmail.com>
2025-01-24 21:38:29 +0200

    player health script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,5 +2,28 @@ using UnityEngine;
 
 public class Player
 {
-    
+    int health = 100;
+
+    public void LoseLife(int damage)
+    {
+        health -= damage;
+        Debug.Log("Player lost health: " + health);
+    }
+
+    public void GainLife(int heal)
+    {
+        health += heal;
+        Debug.Log("Player gained health: " + health);
+    }
+
+    public void Die()
+    {
+        Debug.Log("Player has died");
+    }
+
+    public void Respawn()
+    {
+        health = 100;
+        Debug.Log("Player has respawned");
+    }
 }

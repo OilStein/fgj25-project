083eb77e32642d3918fd9c7d79432b1402e7852c
Winded <antton.hytonen@outlook.com>
2025-01-25 14:14:20 +0200

    Dialog series and other shit

---
+++ Assets/Scripts/DialogSeries.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEditor;
+using UnityEngine;
+
+public class DialogSeries : ScriptableObject
+{
+    [MenuItem("Assets/Create/DialogSeries")]
+    public static void Create()
+    {
+        var asset = CreateInstance<DialogSeries>();
+
+        AssetDatabase.CreateAsset(asset, "Assets/NewDialogSeries.asset");
+        AssetDatabase.SaveAssets();
+
+        EditorUtility.FocusProjectWindow();
+
+        Selection.activeObject = asset;
+    }
+
+    [Serializable]
+    public struct DialogEntry
+    {
+        public AudioClip AudioClip;
+        public string SubtitleText;
+    }
+
+    public DialogEntry[] Entries;
+}
